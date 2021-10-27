0fe08cc92ac8677eba8f4c53aa5b5fa7957cc5cc
Pouriya Jafarpur <poj000@ib12be-075.science.gc.ca>
2021-10-27 22:05:32 +0000

    making 1 netcdf file per model per ssp for all vaiables

--- CMIP6_test.py
+++ CMIP6_test.py
@@ -11,37 +11,51 @@ import glob, os
 import numpy as np
 import pandas as pd
 import time
+from datetime import timedelta
 import dask
+
+start_time = time.time()
 
 # Name of the variable, the ssp and model 
 ssp = ['ssp126','ssp245','ssp585']
 
 variables = ['tasmax', 'pr', 'tasmin']
 """
-models = ['ACCESS-CM2', 'ACCESS-ESM1-5', 'BCC-CSM2-MR', 'CNRM-CM6-1',
-          'CNRM-ESM2-1', 'CanESM5', 'FGOALS-g3', 'GFDL-ESM4', 'HadGEM3-GC31-LL',
+models = ['ACCESS-CM2', 'ACCESS-ESM1-5', 'BCC-CSM2-MR', 'CMCC-ESM2', 'CNRM-CM6-1',
+          'CNRM-ESM2-1', 'CanESM5', 'EC-Earth3-Veg', 'EC-Earth3', 'FGOALS-g3', 'GFDL-ESM4', 'HadGEM3-GC31-LL',
           'INM-CM4-8', 'INM-CM5-0', 'IPSL-CM6A-LR', 'KACE-1-0-G', 'KIOST-ESM',
-           'MIROC-ES2L', 'MIROC6']
+           'MIROC-ES2L', 'MIROC6', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR', 'MRI-ESM2-0', 'NorESM2-LM', 
+           'NorESM2-MM', 'TaiESM1', 'UKESM1-0-LL']          # total of 26 models
 
 """
-#models = ['ACCESS-CM2', 'ACCESS-ESM1-5']
+models = ['BCC-CSM2-MR', 'ACCESS-ESM1-5']
 
-models = ['ACCESS-ESM1-5']
+#models = ['ACCESS-ESM1-5']
 
 list_netcdf=[]
 
 for mods in models:
-    for var in variables:
-            for s in ssp:
-                Input = glob.glob('/***REMOVED***/ccdp/scrd901/CMIP6_bccaqv2datasets/'+mods+'_'+var+'*_'+s+'_5_year_files')
-                #print(Input)
-                for folder in Input[:]:
-                    file_list=os.listdir(folder)
-                    print('working on folder:' + folder)
-                    file_list=[folder+'/'+f for f in file_list if '.nc' in f and not 'foQXts' in f]
-                    #print(file_list)
-                    ds=xr.open_mfdataset(file_list)
-                    with dask.config.set(**{'array.slicing.split_large_chunks': False}):
-                        Annual_mean = ds.groupby('time.year').mean(dim=('lon','lat','time'))
-                        print(Annual_mean) #printing
+    for s in ssp:
+        for var in variables:
+            Input = glob.glob('/***REMOVED***/ccdp/scrd901/CMIP6_bccaqv2datasets/'+mods+'_'+var+'*_'+s+'_5_year_files')
+            #print(Input)
+            for folder in Input[:]:
+                file_list=os.listdir(folder)
+                file_list.sort()
+                print('working on folder:' + folder)
+                file_list=[folder+'/'+f for f in file_list if '.html' not in f and not '.nc.' in f]
+                print(file_list)
+                ds=xr.open_mfdataset(file_list, parallel=True)
+                
+tair_max = xr.DataArray(data_variables= ,
+                        coords=[years, lat, lon], 
+                        dims=dimensions,
+                        attrs=metadata)
+
+
+#final_dataset = new_dataset.groupby('time.year').mean('time')                            # or 'lat' and 'lon' to get timeseries
+#print(final_dataset)
+#final_dataset.to_netcdf('/***REMOVED***/***REMOVED***/CMIP6_Test/ssp126/'+mods+'_'+s+'_150_year_mean.nc')
+
+print("It took", str(timedelta(seconds=time.time() - start_time)), "to run")
                             
